167d30a6314552a57b4b9efe767d6b7ba4f7b3fd
woojin9222 <woojin9222@gmail.com>
2024-06-14 01:04:22 +0900

    main

---
+++ app/login/page.js
@@ -0,0 +1,73 @@
+'use client';
+import Link from "next/link";
+import supabase from "../utils/supabase/server.js";
+import { useState } from 'react';
+
+
+
+export default function login() {
+
+    
+    const handleGoogleSignIn = async (e) => {
+        e.preventDefault(); // Prevent default form submission
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'https://coreachat.vercel.app/',
+      },
+    })
+    };
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault(); // Prevent default form submission
+        const { email, password } = formData;
+    
+        try {
+          const { error } = await supabase.auth.signInWithPassword({ email, password });
+    
+          if (error) {
+            console.error('Error signing in:', error.message);
+            alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인하세요.');
+          } else {
+            console.log('Login successful');
+            // 로그인 성공 후 리다이렉트 또는 다른 로직 추가
+          }
+        } catch (error) {
+          console.error('Error signing in:', error.message);
+          alert('로그인 중 문제가 발생했습니다.');
+        }
+      };
+
+    return(
+        <div className="SignUp">
+            <div className="SignUpList">
+                <span className="logo">고구려채팅</span>
+                <form className="form" onSubmit={handleSubmit}>
+
+                    <input className="input" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="이메일"/>
+
+                    <input className="input" type="password" name="password" value={formData.password} onChange={handleChange} placeholder="비밀번호"/>
+                    <button className="button" type="submit">로그인하기</button>
+                    <button className="button" onClick={handleGoogleSignIn}>
+                            구글로 로그인하기
+                    </button>
+
+                </form>
+                <p><Link href="/signup">아직계정이 없으신가요? 회원가입하기</Link></p>
+            </div>
+        </div>
+    );
+
+} 
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import {useEffect}  from 'react';
+import {useEffect, useState}  from 'react';
 import Link from "next/link"
 import supabase from "../utils/supabase/server.js";
 
@@ -10,29 +10,74 @@ export default function SignUp() {
     const handleGoogleSignIn = async (e) => {
             e.preventDefault(); // Prevent default form submission
         await supabase.auth.signInWithOAuth({
-          provider: google,
+          provider: 'google',
           options: {
-            redirectTo: 'https://coreachat.vercel.app/`,
+            redirectTo: 'https://coreachat.vercel.app/',
           },
         })
 
 
+      };
+
+      const [formData, setFormData] = useState({
+        displayName: '',
+        email: '',
+        password: '',
+        file: null,
+      });
+
+      const handleChange = (e) => {
+        const { name, value, files } = e.target;
+        setFormData({
+          ...formData,
+          [name]: files ? files[0] : value,
+        });
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { displayName, email, password, file } = formData;
+    
+        try {
+          const { error } = await supabase.auth.signUp({
+            email: email ,
+            password: password,
+            options: {
+              emailRedirectTo: 'https://coreachat.vercel.app/',
+              data: {
+                displayName: displayName,
+              },
+            },
+          });
+    
+          if (error) {
+            if (error.message.includes('Email rate limit exceeded')) {
+              console.error('Too many sign-up attempts. Please try again later.');
+            } else {
+              throw error;
+            }
+          } else {
+            console.log('User signed up successfully');
+          }
+        } catch (error) { 
+          console.error('Error signing up:', error.message);
+        }
       };
       
     return(
         <div className="SignUp">
             <div className="SignUpList">
                 <span className="logo">고구려채팅</span>
-                <form className="form">
-                    <input className="input" type="text" placeholder="당신의 닉네임"/>
+                <form className="form" onSubmit={handleSubmit}>
+                    <input className="input" type="text" name="displayName" value={formData.displayName} onChange={handleChange} placeholder="당신의 닉네임"/>
 
-                    <input className="input" type="email" placeholder="이메일"/>
+                    <input className="input" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="이메일"/>
 
-                    <input className="input" type="password" placeholder="비밀번호"/>
+                    <input className="input" type="password" name="password" value={formData.password} onChange={handleChange} placeholder="비밀번호"/>
 
-                    <input style={{display:"none"}} type="file" id="file"/>
+                    <input style={{display:"none"}} type="file" name="file" onChange={handleChange} id="file"/>
                     <label style={{ fontSize: "12px",  color: "black", cursor: "pointer"}} htmlFor="file">당신의 프로필사진</label>
-                    <button className="button">가입하기</button>
+                    <button className="button" type='submit'>가입하기</button>
                     <button className="button" onClick={handleGoogleSignIn}>
                             구글로 로그인하기
                     </button>
